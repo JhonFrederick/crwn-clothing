312547fa3c44afdaac83e56dfa7bb4398685b0eb
Jhon Frederick Mena R <jhonmr2010@hotmail.es>
2024-03-14 23:46:14 -0500

    Taking data from firebase, add categoriesContext and CategoryPreview component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,23 @@
-import {useContext} from "react";
-import {ProductsContext} from "../../contexts/products.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import {Fragment, useContext} from "react";
+import {CategoriesContext} from "../../contexts/categories.context";
 
 import "./shop.styles.scss.css"
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 const Shop = () => {
-    const {products} = useContext(ProductsContext);
-    return (
-        <div className="products-container">
-            {
-                products.map((product) => {
-                    return(
-                        <ProductCard key={product.id} product={product} />
-                    )
-                })
-            }
-        </div>
-    )
+  const {categoriesMap} = useContext(CategoriesContext);
+  return (
+    <div className="shop-container">
+      {
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title];
+          return (
+            <CategoryPreview key={title} title={title} products={products}/>
+          )
+        })
+      }
+    </div>
+  );
 }
 
 export default Shop;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,24 +1,24 @@
 import {initializeApp} from "firebase/app";
 import {
-    getAuth,
-    signInWithRedirect,
-    signInWithPopup,
-    GoogleAuthProvider,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    onAuthStateChanged
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from "firebase/auth";
-import {getFirestore, doc, getDoc, setDoc} from "firebase/firestore"
+import {getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs} from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDpRA2ld3tiGqx8Puvg9xklg6wduulyIZc",
-    authDomain: "crwn-clothing-db-45f19.firebaseapp.com",
-    projectId: "crwn-clothing-db-45f19",
-    storageBucket: "crwn-clothing-db-45f19.appspot.com",
-    messagingSenderId: "1085559907552",
-    appId: "1:1085559907552:web:372739eced8ccb01d5dc85"
+  apiKey: "AIzaSyDpRA2ld3tiGqx8Puvg9xklg6wduulyIZc",
+  authDomain: "crwn-clothing-db-45f19.firebaseapp.com",
+  projectId: "crwn-clothing-db-45f19",
+  storageBucket: "crwn-clothing-db-45f19.appspot.com",
+  messagingSenderId: "1085559907552",
+  appId: "1:1085559907552:web:372739eced8ccb01d5dc85"
 };
 
 // Initialize Firebase
@@ -26,7 +26,7 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({
-    prompt: "select_account"
+  prompt: "select_account"
 });
 
 export const auth = getAuth();
@@ -35,38 +35,60 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 export const db = getFirestore();
 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+  await batch.commit();
+  console.log("done");
+}
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "categories");
+  const q = query(collectionRef);
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const {title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {})
+
+  return categoryMap;
+};
+
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
-    if (!userAuth) return;
-    const userDocRef = doc(db, "users", userAuth.uid);
+  if (!userAuth) return;
+  const userDocRef = doc(db, "users", userAuth.uid);
 
-    const userSnapshot = await getDoc(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
 
-    if (!userSnapshot.exists()) {
-        const {displayName, email} = userAuth;
-        const createdAt = new Date();
+  if (!userSnapshot.exists()) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
 
-        try {
-            await setDoc(userDocRef,
-                {
-                    displayName, email, createdAt,
-                    ...additionalInformation
-                });
-        } catch (error) {
-            console.log("Error creating the user", error.message);
-        }
+    try {
+      await setDoc(userDocRef, {
+        displayName, email, createdAt, ...additionalInformation
+      });
+    } catch (error) {
+      console.log("Error creating the user", error.message);
     }
-    return userDocRef;
+  }
+  return userDocRef;
 
 }
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-    if (!email || !password) return;
-    return await createUserWithEmailAndPassword(auth, email, password);
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
 }
 
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-    if (!email || !password) return;
-    return await signInWithEmailAndPassword(auth, email, password);
+  if (!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password);
 }
 
 export const signOutUser = async () => await signOut(auth);
